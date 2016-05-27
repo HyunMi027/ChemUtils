379c9a62791b34f6b1b6296756d4c794092594d1
HyunMi <ketsi027@gmail.com>
2016-05-27 23:54:14 +0200

    :herb:

--- ChemUtils/MainWindow.xaml.cs
+++ ChemUtils/MainWindow.xaml.cs
@@ -3,68 +3,80 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.IO;
 using System.Runtime.CompilerServices;
+using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Input;
 using ChemUtils.Annotations;
 using ChemUtils.Models;
 using MahApps.Metro.Controls;
+using Microsoft.Win32;
 using Newtonsoft.Json;
-using PropertyChanged;
 
 namespace ChemUtils
 {
     public partial class MainWindow : MetroWindow, INotifyPropertyChanged
     {
+        private int _selectedTab = 0;
+        public int SelectedTab
+        {
+            get { return this._selectedTab; }
+            set
+            {
+                this._selectedTab = value;
+                this.OnPropertyChanged();
+            }
+        }
+
         public List<Atom> Atoms { get; } = JsonConvert.DeserializeObject<List<Atom>>(File.ReadAllText(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Atoms.json")));
 
-        private Atom _previewAtom;
-        public Atom PreviewAtom
-        {
-            get { return this._previewAtom; }
-            set
-            {
-                this._previewAtom = value;
-                this.OnPropertyChanged();
-            }
-        }
-
-        private Atom _selectedAtom;
-        public Atom SelectedAtom
-        {
-            get { return this._selectedAtom; }
-            set
-            {
-                this._selectedAtom = value;
-                this.OnPropertyChanged();
-            }
-        }
-
-        private string _filterQuery = null;
-        public string FilterQuery
-        {
-            get { return this._filterQuery; }
-            set
-            {
-                this._filterQuery = value;
-                this.OnPropertyChanged();
-            }
-        }
+        private HashSet<string> selectedSafetysheets = new HashSet<string>();
 
         public MainWindow()
         {
             this.InitializeComponent();
+
+            this.Loaded += delegate
+            {
+                // TODO: move to XAML, :cat_lazy:
+                var files = Directory.EnumerateFiles(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Resources", "Substancesheets"));
+                foreach (string file in files)
+                {
+                    CheckBox checkBox = new CheckBox()
+                    {
+                        Content = Path.GetFileName(file).Replace(".pdf", ""),
+                        Style = (Style)this.FindResource("MaterialDesignAccentCheckBox")
+                    };
+                    checkBox.Checked += delegate
+                    {
+                        this.selectedSafetysheets.Add(file);
+                    };
+                    checkBox.Unchecked += delegate
+                    {
+                        if (this.selectedSafetysheets.Contains(file))
+                        {
+                            this.selectedSafetysheets.Remove(file);
+                        }
+                    };
+                    this.SafetysheetsListView.Items.Add(checkBox);
+                }
+            };
         }
 
-        private void OpenInformationCard(object sender, MouseButtonEventArgs e)
+        private void SaveSafetySheets(object sender, RoutedEventArgs e)
         {
-            this.PreviewAtom = (Atom) sender;
-            // TODO: open atom information card popup
+            SaveFileDialog dialog = new SaveFileDialog
+            {
+                OverwritePrompt = true,
+                Title = "Save file",
+                AddExtension = true,
+                FileName = "Werkplekinstructiekaarten.pdf"
+            };
+            dialog.ShowDialog();
+
+            var stream = dialog.OpenFile();
+            // TODO: combine pdfs and write dem data to stream
         }
 
-        private void FilterQueryChanged(object sender, TextChangedEventArgs e)
-        {
-            string query = ((TextBox) sender).Text.ToLowerInvariant();
-        }
+        private void SwitchTab(object sender, RoutedEventArgs e) => this.SelectedTab = int.Parse((string)((Button)sender).Tag);
 
         public event PropertyChangedEventHandler PropertyChanged;
 
